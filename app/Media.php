48067501dd8cb45014af163264a5bbd30ccb709c
René Kooi <rene@kooi.me>
2015-08-26 19:49:54 +0200

    add SoundCloud embeds

--- app/Media.php
+++ app/Media.php
@@ -4,11 +4,37 @@ namespace App;
 
 class Media extends Model
 {
+    const YOUTUBE = 1;
+    const SOUNDCLOUD = 2;
+
     protected $collection = 'media';
 
     public function getFullTitleAttribute()
     {
         return $this->author . ' - ' . $this->title;
+    }
+
+    public function getEmbeddableAttribute()
+    {
+        return $this->format === Media::YOUTUBE || $this->format === Media::SOUNDCLOUD;
+    }
+
+    public function getEmbedUrlAttribute()
+    {
+        if ($this->format === Media::YOUTUBE) {
+            return 'https://www.youtube-nocookie.com/embed/' . $this->cid;
+        } else if ($this->format === Media::SOUNDCLOUD) {
+            $apiUrl = 'https://api.soundcloud.com/tracks/' . $this->cid;
+            return 'https://w.soundcloud.com/player/?' . http_build_query([
+                'url' => $apiUrl,
+                'auto_play' => 'false',
+                'hide_related' => 'false',
+                'show_comments'=> 'true',
+                'show_user' =>'true',
+                'show_reposts' => 'false',
+                'visual' => 'true'
+            ]);
+        }
     }
 
     public function history()
--- resources/views/media/show.blade.php
+++ resources/views/media/show.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <div class="media">
         <h1>{{ $media->full_title }}</h1>
+
+        <div class="embed">
+            @if ($media->embeddable)
+                <iframe width="800"
+                        height="450"
+                        src="{{ $media->embed_url }}"
+                        scrolling="no"
+                        frameborder="0"
+                        allowfullscreen></iframe>
+            @endif
+        </div>
+
         <p>
             This song was played {{ $history->total() }} times.
         </p>
@@ -12,16 +24,6 @@
                 loves this song the most: they played it {{ $loverCount }} times.
             </p>
         @endif
-        <div class="embed">
-            @if ($media->format === 1)
-                <iframe width="560"
-                        height="315"
-                        src="https://www.youtube-nocookie.com/embed/{{ $media->cid }}"
-                        frameborder="0"
-                        allowfullscreen>
-                </iframe>
-            @endif
-        </div>
 
         <h2>Play History</h2>
         @include('history.list', ['entries' => $history])
