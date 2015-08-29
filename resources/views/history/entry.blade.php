63f22d30248bb023b61d47c406f8ab6548ca626e
René Kooi <rene@kooi.me>
2015-08-29 20:00:21 +0200

    refactor history item styles

--- resources/views/history/entry.blade.php
+++ resources/views/history/entry.blade.php
@@ -1,6 +1,6 @@
-<div class="history-list-row" data-entry-id="{{ $entry->id }}">
-    <img src="{{ $entry->mediaM->image }}" alt="Image">
-    <div class="meta">
+<div class="media-item history-list-row" data-entry-id="{{ $entry->id }}">
+    <img class="media-item-image" src="{{ $entry->mediaM->image }}" alt="Image">
+    <div class="media-item-meta">
         <a class="media" href="{{ url('media', $entry->mediaM->cid) }}">
             {{ $entry->mediaM->full_title }}
         </a>
@@ -15,17 +15,17 @@
             {{ $entry->time or '0000-00-00 00:00:00' }}
         </time>
     </div>
-    <div class="score">
-        <div class="woots">
+    <div class="media-score history-list-row-score">
+        <div class="media-score-type media-score-woots">
             <i class="icon-woot"></i><span>{{ $entry->score->positive or '?' }}</span>
         </div>
-        <div class="grabs">
+        <div class="media-score-type media-score-grabs">
             <i class="icon-grab"></i><span>{{ $entry->score->grabs or '?' }}</span>
         </div>
-        <div class="mehs">
+        <div class="media-score-type media-score-mehs">
             <i class="icon-meh"></i><span>{{ $entry->score->negative or '?' }}</span>
         </div>
-        <div class="listeners">
+        <div class="media-score-type media-score-listeners">
             <i class="icon-listeners"></i><span>{{ $entry->score->listeners or '?' }}</span>
         </div>
     </div>
