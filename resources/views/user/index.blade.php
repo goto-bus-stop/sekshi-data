db7162ce5410e4c074e398598a73156eb38c0b98
René Kooi <rene@kooi.me>
2015-09-05 19:53:34 +0200

    remove old user list stuff

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,30 +6,6 @@
     <div class="UserList">
         @each('user.entry', $users, 'user')
     </div>
-{{--
-    <table class="users">
-        <thead>
-            <tr>
-                <th>Username</th>
-                <th>Plays</th>
-                <th>Karma</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr>
-                    @if ($user)
-                        <td><a href="{{ user_url($user) }}">{{ $user->username }}</a></td>
-                        <td>{{ $user->history()->count() }}</td>
-                        <td>{{ $user->karma()->sum('amount') }}</td>
-                    @else
-                        <td colspan="3">Deleted user</td>
-                    @endif
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
---}}
 
     {!! $users->render() !!}
 @endsection
