9fcf8e4f590747aa6b74b7c1736ac68c5b8c404c
René Kooi <rene@kooi.me>
2015-09-05 19:50:00 +0200

    add sort URL helper

--- app/helpers.php
+++ app/helpers.php
@@ -17,6 +17,24 @@ if (!function_exists('duration')) {
     }
 }
 
+if (!function_exists('sort_url')) {
+    function sort_url($prop, $default)
+    {
+        static $inverse = [
+            'asc' => 'desc',
+            'desc' => 'asc'
+        ];
+        $current = Request::input('sort');
+        $order = Request::input('order', $default);
+        $query = Request::except('page', 'order');
+        if ($current === $prop) {
+            $query['order'] = $inverse[$order];
+        }
+        $query['sort'] = $prop;
+        return '?' . http_build_query($query);
+    }
+}
+
 if (!function_exists('user_url')) {
     function user_url(User $user)
     {
