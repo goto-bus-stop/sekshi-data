ab2aa64b8f6aef348c39514018bc0df055e60748
René Kooi <rene@kooi.me>
2015-09-05 19:53:43 +0200

    add sort header to user list

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,13 @@
     {!! $users->render() !!}
 
     <div class="UserList">
+        <div class="UserList-header UserRow">
+            <div class="UserRow-info">
+                <a href="{{ sort_url('user', 'asc') }}" class="UserRow-username">Username</a>
+                <a href="{{ sort_url('plays', 'desc') }}" class="UserRow-playcount">Plays</a>
+                <a href="{{ sort_url('karma', 'desc') }}" class="UserRow-karma">Karma</a>
+            </div>
+        </div>
         @each('user.entry', $users, 'user')
     </div>
 
