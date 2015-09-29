fd64efea3760fe1089d932607246ee3cc09271fb
René Kooi <rene@kooi.me>
2015-09-29 16:11:10 +0200

    add global nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,25 @@
     <link rel="stylesheet" href="{{ asset('css/badges.css') }}">
 </head>
 <body>
+    <nav class="Navigation">
+        <ul class="NavList">
+            <li class="NavItem NavItem--brand">
+                SekshiBot Data
+            </li>
+            <li class="NavItem">
+                <a href="{{ action('UserController@index') }}">Users</a>
+            </li>
+            <li class="NavItem">
+                <a href="{{ action('HistoryController@index') }}">Play History</a>
+            </li>
+            <li class="NavItem">
+                <a href="{{ action('HistoryController@mostPlayed') }}">Most Played Songs</a>
+            </li>
+            <li class="NavItem">
+                <a href="{{ action('MediaController@index') }}">Known Media</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
